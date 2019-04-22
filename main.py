26cfaa8658dbca6135f92acb69b780870f994753
mortackai <mattraymon@gmail.com>
2019-04-22 15:15:46 -0400

    split files

---
+++ main.py
@@ -0,0 +1,48 @@
+import module as M
+
+
+M.menu.display_menu("Main Menu", "Single Player", "Multi-Player")
+
+print(M.menu.display_menu.selection)
+
+'''
+if M.chosen == 0:
+    M.menu.display_menu("difficulty", "normal", "Hard")
+if M.chosen == 1:
+    M.menu.display_menu("Game Mode", "Split-Screen", "Host", "Join")
+'''
+
+
+
+'''main menu
+game mode
+single player
+multi player
+'''
+
+'''single player
+difficulty
+normal
+fast
+'''
+
+'''multiplayer
+splitscreen
+host
+join
+'''
+
+'''Splitscreen
+ball speed
+normal
+fast
+'''
+
+'''host
+"now hosting"
+'''
+
+'''join
+"enter IP of host"
+"connecting"
+'''
