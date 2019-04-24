2416427c8212e955eb5fcd1bb5684ef9c616ca8d
mortackai <mattraymon@gmail.com>
2019-04-24 13:38:34 -0400

    return to main menu and exit

--- menu.py
+++ menu.py
@@ -1,11 +1,19 @@
 # these inputs needs to be sanitized
-
+import os
 
 def menu():
+    game_mode_chosen = 0
+    multi_mode_chosen = 0
     print("Select Game Mode")
     print("1) Single Player")
     print("2) Multiplayer")
+    print("98) Main Menu")
+    print("99) Exit")
     game_mode_chosen = int(input())
+    if game_mode_chosen == 98:
+        menu()
+    if game_mode_chosen == 99:
+        os._exit(0)
 
 # single player - difficulty
     if game_mode_chosen == 1:
@@ -21,9 +29,15 @@ def menu():
         print("1) Splitscreen")
         print("2) Host")
         print("3) Join")
+        print("98) Main Menu")
+        print("99) Exit")
         multi_mode_chosen = int(input())
         if multi_mode_chosen == 1:
             multi_local_done = False
+        if multi_mode_chosen == 98:
+            menu()
+        if multi_mode_chosen == 99:
+            os._exit(0)
 
 # host
     if multi_mode_chosen == 2:
