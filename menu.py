d82595b55a7ce09a46741b68147ab03334b8265a
mortackai <mattraymon@gmail.com>
2019-04-29 12:48:33 -0400

    chnaged to functions

--- menu.py
+++ menu.py
@@ -1,54 +1,65 @@
-# these inputs needs to be sanitized
+# inputs needs to be sanitized
 import os
 
-def menu():
-    game_mode_chosen = 0
-    multi_mode_chosen = 0
+def main_menu():
     print("Select Game Mode")
     print("1) Single Player")
     print("2) Multiplayer")
     print("98) Main Menu")
     print("99) Exit")
-    game_mode_chosen = int(input())
-    if game_mode_chosen == 98:
-        menu()
-    if game_mode_chosen == 99:
+    choice = int(input())
+    if choice == 1:
+        difficulty_menu()
+    if choice == 2:
+        multiplayer_menu()
+    if choice == 98:
+        main_menu()
+    if choice == 99:
         os._exit(0)
+	
 
 # single player - difficulty
-    if game_mode_chosen == 1:
-        print("")
-        difficulty = int(input("Enter a difficulty 1-9: "))
-        done = False
+def difficulty_menu():
+    print("")
+    difficulty = int(input("Enter a difficulty 1-9: "))
+    done = False
+    return difficulty
 
 # multiplayer - mode
-    if game_mode_chosen == 2:
-        print("")
-        print("Multiplayer")
-        print("")
-        print("1) Splitscreen")
-        print("2) Host")
-        print("3) Join")
-        print("98) Main Menu")
-        print("99) Exit")
-        multi_mode_chosen = int(input())
-        if multi_mode_chosen == 1:
-            multi_local_done = False
-        if multi_mode_chosen == 98:
-            menu()
-        if multi_mode_chosen == 99:
-            os._exit(0)
+def multiplayer_menu():
+    print("")
+    print("Multiplayer")
+    print("")
+    print("1) Splitscreen")
+    print("2) Host")
+    print("3) Join")
+    print("98) Main Menu")
+    print("99) Exit")
+    choice = int(input())
+    if choice == 1:
+        difficulty_menu()
+        multi_local_done = False
+    if choice == 2:
+        host_menu()
+    if choice == 3:
+        join_menu()
+    if choice == 98:
+       main_menu()
+    if choice == 99:
+        os._exit(0)
 
 # host
-    if multi_mode_chosen == 2:
-        print("")
-        print("Host")
-        print("waiting for connection...")
+def host_menu():
+    print("")
+    print("Host")
+    print("waiting for connection...")
 
 # join
-    if multi_mode_chosen == 3:
-        print("")
-        print("Join")
-        print("")
-        host_ip = input("Enter host IP address: ")
-        print("connecting...")
+def join_menu():
+    print("")
+    print("Join")
+    print("")
+    host_ip = input("Enter host IP address: ")
+    print("connecting...")
+
+main_menu()
